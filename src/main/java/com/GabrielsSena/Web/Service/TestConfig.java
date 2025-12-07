95b2c58df79a3f0388018cdbdd0cf6f22b8b5686
Gabriel Sena <gabrielsena10072@2gmail.com>
2025-12-07 10:38:09 -0300

    Criando o ambiente de testes. Alterei o application.properties e adicionei o application-test.properties

---
+++ src/main/java/com/GabrielsSena/Web/Service/TestConfig.java
@@ -0,0 +1,32 @@
+package com.GabrielsSena.Web.Service;
+
+import com.GabrielsSena.Web.Service.Entities.User;
+import com.GabrielsSena.Web.Service.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public TestConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
+}
